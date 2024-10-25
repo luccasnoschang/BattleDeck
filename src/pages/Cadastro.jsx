e68acc764d5334968510b159a5fd646b1a2ef7a3
LUCCAS OLIVEIRA NOSCHANG <luccas_noschang@estudante.sesisenai.org.br>
2024-10-24 21:51:23 -0300

    att

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -4,14 +4,43 @@ import "./Cadastro.css"
 function Cadastro() {
     return (
         <div className='container'>
-            <img className='imgFundo' src="./src/assets/images/imgCadastro.png" alt=""/>
+            {/* <img className='imgFundo' src="./src/assets/images/imgCadastro.png" alt="" /> */}
 
-            <div className='divLogo'>
-                <img src="./src/assets/images/LogoShadowDuel.png" alt="" />
+            <div className='parteCima'>
+
+                <div className='divLogo'>
+                    <img className='Logo' src="./src/assets/images/LogoShadowDuel.png" alt="" />
+                    <img className='shadowDuel' src="./src/assets/images/shadowDuel.png" alt="" />
+                </div>
             </div>
 
-            <div className='divCadastrar'>
-                <p>teste</p>
+            <div className='parteBaixo'>
+                <div className='divCadastrar'>
+                  
+                  <div className='textoInicial'>
+                    <h1 className='titulo'>Crie a sua conta</h1>
+                    <p className='subTitulo'>Por favor, preencha seus dados para criar uma conta</p>
+                  </div>
+
+                    <div className='divDescricao'>
+                        <p className='descricaoInput'>Seu apelido*</p>
+                        <input type="text" />
+                    </div>
+
+                    <div className='divDescricao'>
+                        <p className='descricaoInput'>Email*</p>
+                        <input type="text" />
+                    </div>
+
+                    <div className='divDescricao'>
+                        <p className='descricaoInput'>Senha*</p>
+                        <input type="text" />
+                    </div>
+
+                    <a href="">Já tem uma conta?</a>
+                    <button>Criar Conta</button>
+
+                </div>
             </div>
 
         </div>
