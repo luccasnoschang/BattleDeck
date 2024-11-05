ea1a132ac1d41b5b7c9beee81a8ede23ff6256e7
ESTEVAO LIMAS JUNIOR <estevao_limas-jun@estudante.sesisenai.org.br>
2024-11-05 20:04:11 -0300

    Atualização da página loja

--- src/pages/Batalhas.jsx
+++ src/pages/Batalhas.jsx
@@ -7,9 +7,7 @@ function Batalhas() {
     <div className='containerBattles'>
       <Navbar/>
 
-        <div className='divMapa'>
-            <img className='mapa' src="./src/assets/images/mapaBatalhas.png" alt="" />
-        </div>
+        
 
     </div>
   )
--- src/pages/Loja.jsx
+++ src/pages/Loja.jsx
@@ -3,12 +3,23 @@ import Navbar from '../components/Navbar'
 import './Loja.css'
 
 function Loja() {
+  
   return (
+    
     <div className='containerLoja'>
       <Navbar/>
-      
-      
+      <div className='divMercador'>
+        <img className='imgMercador' src="./src/assets/images/mercador.png" alt="Imagem Mercador" />
 
+      </div>
+      <div className='divDeck'>
+
+        <div className='divDecks'><img className='imgDecks' src="./src/assets/images/LogoShadowDuel.png" alt="" /></div>
+        <div className='divDecks'><img className='imgDecks' src="./src/assets/images/LogoShadowDuel.png" alt="" /></div>
+        <div className='divDecks'><img className='imgDecks' src="./src/assets/images/LogoShadowDuel.png"/></div>
+        <div className='divDecks'><img className='imgDecks' src="./src/assets/images/LogoShadowDuel.png"/></div>
+
+      </div>
     </div>
   )
 }
