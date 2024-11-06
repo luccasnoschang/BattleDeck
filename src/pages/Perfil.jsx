dd39118bc78c5ad938d9592d1cea0b7b5189231a
VInicios <ferreiradossantosvinicios@gmail.com>
2024-11-05 21:30:35 -0300

    Atualização perfil

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -5,17 +5,29 @@ import './Perfil.css'
 function Perfil() {
   return (
     <div className='containerPerfil'>
-      <Navbar/>
+      <Navbar />
 
-    <div className='conteudoPerfil'>
+      <div className='paginaPerfil'>
 
-    <img className='ImagemPerfil' src="./src/assets/images/QuadroPerfil.png"  alt="BackgroundPerfil" />
-
-
-
-    </div>
+        <div className='conteudoPerfil'>
 
 
+          <img className='imagemLogoPerfil' src="./src/assets/images/LogoShadowDuel.png" alt="" />
+
+          <div className='usernameTexto'>
+            <h1 className='TextoNomePerfil'> Username:</h1>
+          </div>
+          <div className='textoPerfil'>
+
+            <h2 className='TextoMoeda'>Moeda:</h2>
+            <h3 className='TextoDecks'>Decks:</h3>
+
+          </div>
+
+        </div>
+
+
+      </div>
     </div>
   )
 }
